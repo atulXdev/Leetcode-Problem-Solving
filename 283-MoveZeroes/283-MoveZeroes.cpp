8ac9156b36fe35b9d534c6f1a1997124d2be0318
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-03 11:36:08 +0530

    File updated at 03/05/2026, 11:35:57

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,18 +1,22 @@
-// Last updated: 03/05/2026, 11:28:47
+// Last updated: 03/05/2026, 11:35:57
 1class Solution {
 2public:
 3    void moveZeroes(vector<int>& nums) {
 4        int j=0;
 5        int n=nums.size();
-6        for(int i=0;i<n;i++){
-7            if(nums[i]!=0){
-8                nums[j++]=nums[i];
-9            }
-10        }
-11
-12        while(j<n){
-13            nums[j]=0;
-14            j++;
-15        }
-16    }
-17};
+6  //placing all non zero elemnt to lhs
+7        for(int i=0;i<n;i++){
+8            if(nums[i]!=0){
+9                nums[j++]=nums[i];
+10            }
+11        }
+12
+13        //placing zeros for the remaining elements
+14
+15        while(j<n){
+16            nums[j]=0;
+17            j++;
+18        }
+19        
+20    }
+21};
