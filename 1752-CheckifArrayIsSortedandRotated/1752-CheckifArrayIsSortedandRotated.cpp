ee64c4ad92275d8d28cf39658b1abc919226497a
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-03 11:16:07 +0530

    File updated at 03/05/2026, 11:15:55

--- 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.cpp
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.cpp
@@ -1,24 +1,25 @@
-// Last updated: 03/05/2026, 11:04:32
+// Last updated: 03/05/2026, 11:15:55
 1class Solution {
 2public:
 3    bool check(vector<int>& nums) {
-4     
-5     int n=nums.size();
-6     int count=0;
-7      
-8      for(int i=0;i<n;i++){
-9        if(nums[i]>nums[(i+1)%n]){
-10            count++;
-11        }
-12      }
-13
-14      if(count<=1){
-15        return true;
-16      }
-17
-18      else{
-19        return false;
-20      }
-21        
-22    }
-23};
+4
+5        int count=0;
+6        int n=nums.size();
+7
+8        for(int i=0;i<n;i++){
+9            if(nums[i]>nums[(i+1)%n]){
+10                count++;
+11
+12            }
+13        }
+14
+15        if(count<=1){
+16            return true;
+17        }
+18
+19        else{
+20            return false;
+21        }
+22        
+23    }
+24};
