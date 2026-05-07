3c328470fb9b9a54f817da8269abd95c7088bff6
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-07 17:00:41 +0530

    File updated at 07/05/2026, 17:00:27

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.cpp
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.cpp
@@ -1,38 +1,39 @@
-// Last updated: 07/05/2026, 16:13:40
+// Last updated: 07/05/2026, 17:00:27
 1class Solution {
 2public:
 3    int singleNonDuplicate(vector<int>& nums) {
-4        //bruteforce:sare element ka left right check krenge agar koi vi equal nhe mila then
-5        //eg your ans
-6
-7        int n=nums.size();
-8        if(n==1){
-9            return nums[0];
-10        }
-11        for(int i=0;i<n;i++){
-12            //for first elemt only right we have
-13            if(i==0){
-14                if(nums[i]!=nums[i+1]){
-15                    return nums[i];
-16                }
-17            }
-18
-19            //if last elememt only left we have
-20           else if(i==n-1){
-21            if(nums[i]!=nums[i-1]){
-22                return nums[i];
-23            }
-24           }
-25
-26           //any middle elemnt we will check left and right both
-27           else{
-28            if(nums[i]!=nums[i-1]&& nums[i]!=nums[i+1]){
-29                return nums[i];
-30            }
-31           }
+4        int n=nums.size();
+5        //handeling all edge cases
+6        if(n==1){
+7            return nums[0];
+8        }
+9       if(nums[0]!=nums[1]){
+10        return nums[0];
+11       } 
+12       if(nums[n-1]!=nums[n-2]){
+13        return nums[n-1];
+14       }
+15       int low=1;
+16       int high=n-2;
+17
+18       while(low<=high){
+19        int mid=(low+high)/2;
+20        //uniq element when left and right are not eqaul
+21        if(nums[mid]!=nums[mid+1]&& nums[mid]!=nums[mid-1] ){
+22            return nums[mid];
+23        }
+24        //if on left half (even,odd) trim the left half
+25        if((mid%2==0 && nums[mid]==nums[mid+1])||(mid%2==1 && nums[mid]==nums[mid-1])){
+26            //trim the left half
+27            low=mid+1;
+28        }
+29        //else you are in right half trim the right half
+30        else{
+31            high=mid-1;
 32        }
-33
-34        return -1;
-35        
-36    }
-37};
+33       }
+34
+35       return -1;
+36       
+37    }
+38};
