a49a9c298e7bc6212d131a47cd2449110da5c471
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-11 12:30:53 +0530

    File updated at 11/05/2026, 12:30:36

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,30 +1,28 @@
-// Last updated: 11/05/2026, 12:14:00
+// Last updated: 11/05/2026, 12:30:36
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        int l1=s.size();
-5        int l2=t.size();
-6        map<char,int> m1;
-7        map<char,int> m2;
-8
-9        if(l1==l2){
-10            for(int i=0;i<l1;i++){
-11                m1[s[i]]++;
-12                m2[t[i]]++;
-13
-14            }
-15
-16            if(m1==m2){
-17                return true;
-18            }
-19
-20            else{
-21                return false;
-22            }
-23           
-24        }
-25
-26            return false;
-27        
-28    }
-29};
+4      if(s.size()!=t.size()){
+5        return false;
+6      }
+7//as all letters in lowercase we can use this
+8      vector<int> freq(26,0);
+9      //this vector should reamin 0 for anagram
+10      //neutal throem =>no spike should be visible
+11
+12      for(int i=0;i<s.size();i++){
+13        freq[s[i]-'a']++;
+14        freq[t[i]-'a']--;
+15      }
+16
+17      for(int count:freq){
+18        if(count!=0){
+19            return false;
+20        }
+21      }
+22
+23      return true;
+24
+25        
+26    }
+27};
