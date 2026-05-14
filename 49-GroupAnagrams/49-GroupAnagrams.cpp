94536f8b0598dd002ce3211ef425419014b36e7c
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-14 16:26:20 +0530

    File updated at 14/05/2026, 16:26:01

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,4 +1,4 @@
-// Last updated: 10/04/2026, 14:52:59
+// Last updated: 14/05/2026, 16:26:01
 1class Solution {
 2public:
 3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -6,15 +6,17 @@
 5
 6        for(string s:strs){
 7            string temp=s;
-8            sort(temp.begin(),temp.end());
-9
-10            mp[temp].push_back(s);
-11        }
+8            sort(s.begin(),s.end());
+9            mp[s].push_back(temp);
+10        }
+11
 12        vector<vector<string>> ans;
-13        for(auto it:mp){
-14        ans.push_back(it.second);
-15        }
-16
-17        return ans;
-18    }
-19};
+13
+14        for(auto it:mp){
+15            ans.push_back(it.second);
+16        }
+17
+18        return ans;
+19        
+20    }
+21};
