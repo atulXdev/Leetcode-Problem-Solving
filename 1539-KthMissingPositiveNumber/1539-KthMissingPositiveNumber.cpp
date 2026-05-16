0800d814aac63de2836637e21d97ba3432781bab
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-16 17:04:39 +0530

    File updated at 16/05/2026, 17:04:19

--- 1539-KthMissingPositiveNumber/1539-KthMissingPositiveNumber.cpp
+++ 1539-KthMissingPositiveNumber/1539-KthMissingPositiveNumber.cpp
@@ -1,22 +1,17 @@
-// Last updated: 16/05/2026, 16:42:25
+// Last updated: 16/05/2026, 17:04:19
 1class Solution {
 2public:
 3    int findKthPositive(vector<int>& arr, int k) {
-4
-5        vector<int> missing;
-6        unordered_set<int> st;
-7        st.insert(arr.begin(),arr.end());
-8
-9int i=1;
-10        while(missing.size()<k){
-11            if(!st.count(i)){
-12                missing.push_back(i);
-13
-14            }
-15            i++;
-16            }
-17        
-18        int ans=missing[k-1];
-19        return ans;
-20    }
-21};
+4        for (int i=0;i<arr.size();i++){
+5            if(arr[i]<=k){
+6                k++;
+7            }
+8            else{
+9                break;
+10            }
+11        }
+12
+13        return k;
+14        
+15    }
+16};
