166a74193c239a259f574e41ca171d1ca22ce8d3
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-16 23:32:59 +0530

    File updated at 16/05/2026, 23:32:38

--- 136-SingleNumber/136-SingleNumber.cpp
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -1,12 +1,15 @@
-// Last updated: 16/05/2026, 23:26:26
+// Last updated: 16/05/2026, 23:32:38
 1class Solution {
 2public:
 3    int singleNumber(vector<int>& nums) {
-4        int ans=0;
-5
-6        for(int num:nums){
-7            ans=ans^num;
-8        }
-9        return ans;
-10    }
-11};
+4
+5        int ans=0;
+6
+7        for(int num:nums){
+8            ans=ans^num;
+9        }
+10
+11        return ans;
+12        
+13    }
+14};
