8b33f3bdd21a427bdea7f970d7f5d3b08d1948aa
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-16 13:35:27 +0530

    File updated at 16/05/2026, 13:35:06

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,15 +1,22 @@
-// Last updated: 16/05/2026, 13:21:33
+// Last updated: 16/05/2026, 13:35:06
 1class Solution {
 2public:
-3    vector<int> runningSum(vector<int>& nums) {
-4        int n=nums.size();
-5        vector<int> ans(n,0);
-6         ans[0]=nums[0];
-7for(int i=1;i<n;i++){
-8    ans[i]=nums[i]+ans[i-1];
-9}
-10
-11return ans;
-12        
-13    }
-14};
+3    int maximumWealth(vector<vector<int>>& accounts) {
+4        int rowL=accounts.size();
+5        int maxWealth=0;
+6        for(int i=0;i<rowL;i++){
+7            int colL=accounts[i].size();
+8            int sum=0;
+9            for(int j=0;j<colL;j++){
+10                sum+=accounts[i][j];
+11
+12
+13            }
+14            maxWealth=max(sum,maxWealth);
+15        }
+16
+17        return maxWealth;
+18
+19        
+20    }
+21};
