8089370d117ce5cec45c2dee4782c3e96c2e6738
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-16 14:33:23 +0530

    File updated at 16/05/2026, 14:33:01

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,25 +1,15 @@
-// Last updated: 16/05/2026, 13:58:59
+// Last updated: 16/05/2026, 14:33:01
 1class Solution {
 2public:
-3    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-4        int n=candies.size();
-5         vector<bool> ans(n,0);
-6
-7         for(int i=0;i<n;i++){
-8            candies[i]+=extraCandies;
-9            int maxi=*max_element(candies.begin(),candies.end());
-10            if(candies[i]==maxi){
-11                ans[i]=true;
-12            }
-13            else{
-14                ans[i]=false;
-15            }
-16            candies[i]-=extraCandies;
-17         }
-18
-19         return ans;
-20
-21
-22        
-23    }
-24};
+3    vector<int> shuffle(vector<int>& nums, int n) {
+4        vector<int> ans;
+5
+6        for(int i=0;i<n;i++){
+7            ans.push_back(nums[i]);
+8            ans.push_back(nums[i+n]);
+9        }
+10
+11        return ans;
+12        
+13    }
+14};
