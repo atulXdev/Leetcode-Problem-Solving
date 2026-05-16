e4e1b2766067aa58b69a8decc29198e343393b02
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-16 17:36:19 +0530

    File updated at 16/05/2026, 17:35:58

--- 1539-KthMissingPositiveNumber/1539-KthMissingPositiveNumber.cpp
+++ 1539-KthMissingPositiveNumber/1539-KthMissingPositiveNumber.cpp
@@ -1,17 +1,22 @@
-// Last updated: 16/05/2026, 17:04:19
+// Last updated: 16/05/2026, 17:35:58
 1class Solution {
 2public:
 3    int findKthPositive(vector<int>& arr, int k) {
-4        for (int i=0;i<arr.size();i++){
-5            if(arr[i]<=k){
-6                k++;
-7            }
-8            else{
-9                break;
-10            }
-11        }
-12
-13        return k;
-14        
-15    }
-16};
+4        int low=0;
+5        int high=arr.size()-1;
+6        while(low<=high){
+7           int mid=(high+low)/2;
+8           //to get how many missing elemnts before this
+9            int missing=arr[mid]-(mid+1);
+10            if(missing<k){
+11                low=mid+1;
+12            }
+13            else{
+14                high=mid-1;
+15            }
+16        }
+17
+18        return k+high+1;
+19        
+20    }
+21};
