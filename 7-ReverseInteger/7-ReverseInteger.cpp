947519befc31d76762efa37f359c619f3d649120
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-16 23:16:52 +0530

    File updated at 16/05/2026, 23:16:32

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,21 +1,24 @@
-// Last updated: 16/05/2026, 23:02:25
+// Last updated: 16/05/2026, 23:16:32
 1class Solution {
 2public:
 3    int reverse(int x) {
-4        int rev=0;
-5
-6        while(x!=0){
-7            int last=x%10;
-8
-9            if(rev>INT_MAX/10 || rev<INT_MIN/10){
-10                return 0;
-11            }
-12
-13            rev=rev*10+last;
-14            x=x/10;
-15        }
-16
-17        return rev;
-18        
-19    }
-20};
+4
+5        int rev=0;
+6
+7        while(x!=0){
+8            int last=x%10;
+9
+10            //overflow
+11
+12            if(rev>INT_MAX/10 || rev<INT_MIN/10){
+13                return 0;
+14            }
+15
+16            rev=rev*10+last;
+17            x=x/10;
+18        }
+19
+20        return rev;
+21        
+22    }
+23};
