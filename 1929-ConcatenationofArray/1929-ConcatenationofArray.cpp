dc134f10ce08ea54289d169d31988b830600d76a
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-16 12:33:36 +0530

    File updated at 16/05/2026, 12:33:15

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,19 +1,17 @@
-// Last updated: 10/05/2026, 22:24:02
+// Last updated: 16/05/2026, 12:33:15
 1class Solution {
 2public:
 3    vector<int> getConcatenation(vector<int>& nums) {
 4        int n=nums.size();
-5        int l=n+n;
+5        vector<int> ans(2*n,0);
 6
-7        vector<int> ans(l,0);
-8
-9        for(int i=0;i<n;i++){
-10            int si=i+n;
-11            ans[i]=nums[i];
-12            ans[si]=nums[i];
-13        }
-14
-15        return ans;
-16        
-17    }
-18};
+7        for(int i=0;i<n;i++){
+8            int sec=i+n;
+9            ans[i]=nums[i];
+10            ans[sec]=nums[i];
+11        }
+12
+13        return ans;
+14        
+15    }
+16};
