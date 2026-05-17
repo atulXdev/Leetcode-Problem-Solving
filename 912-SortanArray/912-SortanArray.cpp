9b5761c6ddb66f0ed331518948f0cb3a77007f03
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-17 19:41:56 +0530

    Create 912-SortanArray

---
+++ 912-SortanArray/912-SortanArray.cpp
@@ -0,0 +1,11 @@
+// Last updated: 17/05/2026, 19:41:34
+1class Solution {
+2public:
+3    vector<int> sortArray(vector<int>& nums) {
+4
+5        sort(nums.begin(),nums.end());
+6
+7        return nums;
+8        
+9    }
+10};
