418a49c92a1afd6c24eab5b29ec6cb42a54f035d
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-18 00:03:45 +0530

    File updated at 18/05/2026, 00:03:23

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,21 +1,20 @@
-// Last updated: 17/05/2026, 23:50:42
+// Last updated: 18/05/2026, 00:03:23
 1class Solution {
 2public:
-3    bool containsDuplicate(vector<int>& nums) {
-4
-5        unordered_set<int> st;
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int,int> mp;
+5        //key:no val:index
 6
-7        for(int n:nums){
-8            if(!st.count(n)){
-9                st.insert(n);
-10            }
-11            else{
-12                return true;
-13            }
-14
+7        for(int i=0;i<nums.size();i++){
+8            int need=target-nums[i];
+9
+10            if(mp.find(need)!=mp.end()){
+11                return {mp[need],i};
+12            }
+13
+14            mp[nums[i]]=i;
 15        }
-16
-17        return false;
-18        
-19    }
-20};
+16        
+17        return {};
+18    }
+19};
