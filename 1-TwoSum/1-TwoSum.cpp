b275c2747795609f26f3077f24209489bb382780
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-17 18:37:54 +0530

    File updated at 17/05/2026, 18:37:32

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,29 +1,19 @@
-// Last updated: 13/05/2026, 23:29:04
+// Last updated: 17/05/2026, 18:37:32
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4
-5        vector<int> ans;
-6        
-7
-8        for(int i=0;i<nums.size();i++){
-9            int sum=0;
-10            for(int j=i+1;j<nums.size();j++){
-11                sum=nums[i]+nums[j];
-12
-13               
+4        vector<int> ans;
+5        for(int i=0;i<nums.size();i++){
+6            for(int j=i+1;j<nums.size();j++){
+7                if(nums[i]+nums[j]==target){
+8                    ans.push_back(i);
+9                    ans.push_back(j);
+10                }
+11
+12            }
+13        }
 14
-15                if(sum==target){ 
-16                ans.push_back(i);
-17                ans.push_back(j);
-18
-19                    return ans;
-20                }
-21
-22            }
-23        }
-24
-25        return {-1,-1};
-26        
-27    }
-28};
+15        return ans;
+16        
+17    }
+18};
