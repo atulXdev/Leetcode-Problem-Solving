14db0c8d09141c77b5ce08a17c365ad350b60485
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-18 22:15:19 +0530

    File updated at 18/05/2026, 22:14:55

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
@@ -1,21 +1,25 @@
-// Last updated: 18/05/2026, 22:04:06
+// Last updated: 18/05/2026, 22:14:55
 1class Solution {
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
 4        int n=nums.size();
-5  vector<int> ans(n,1);
-6  int  prefix=1;
-7  for(int i=0;i<n;i++){
-8    ans[i]=prefix;
-9    prefix*=nums[i];
-10  }
-11
-12  int suffix=1;
-13  for(int i=n-1;i>=0;i--){
-14    ans[i]*=suffix;
-15    suffix*=nums[i];
-16  }
-17
-18  return ans;
-19    }
-20};
+5
+6        vector<int> ans(n,1);
+7        //in ans vector we will store sprefix product
+8
+9int prefix=1;
+10        for(int i=0;i<n;i++){
+11            ans[i]=prefix;
+12            prefix*=nums[i];
+13        }
+14
+15        int suffix=1;
+16        for(int i=n-1;i>=0;i--){
+17            ans[i]*=suffix;
+18            suffix*=nums[i];
+19        }
+20
+21        return ans;
+22        
+23    }
+24};
