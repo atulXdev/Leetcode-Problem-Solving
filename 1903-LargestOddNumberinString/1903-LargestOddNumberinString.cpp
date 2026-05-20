8f1653af08e7f5c7bc3d4dd87629551ee5579734
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-20 23:20:40 +0530

    File updated at 20/05/2026, 23:20:14

--- 1903-LargestOddNumberinString/1903-LargestOddNumberinString.cpp
+++ 1903-LargestOddNumberinString/1903-LargestOddNumberinString.cpp
@@ -1,19 +1,21 @@
-// Last updated: 20/05/2026, 22:29:12
+// Last updated: 20/05/2026, 23:20:14
 1class Solution {
 2public:
 3    string largestOddNumber(string num) {
 4
-5 int n=num.size();
-6 for(int i=n-1;i>=0;i--){
-7    int lastN=num[i]-'0';
-8    if(lastN%2==0){
-9        num.pop_back();
-10    }
-11    else{
-12        return num;
-13    }
-14 }
-15
-16 return "";       
-17    }
-18};
+5        int n=num.size();
+6
+7        for(int i=n-1;i>=0;i--){
+8            int lastN=num[i]-'0';
+9            if(lastN%2==0){
+10                num.pop_back();
+11            }
+12            else{
+13                return num;
+14            }
+15        }
+16
+17        return "";
+18        
+19    }
+20};
