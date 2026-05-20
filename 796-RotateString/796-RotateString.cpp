6e0fd11fd29a86b3d109a692de4af96ebf8d9a41
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-20 23:10:12 +0530

    File updated at 20/05/2026, 23:09:45

--- 796-RotateString/796-RotateString.cpp
+++ 796-RotateString/796-RotateString.cpp
@@ -1,21 +1,21 @@
-// Last updated: 20/05/2026, 23:04:22
+// Last updated: 20/05/2026, 23:09:45
 1class Solution {
 2public:
 3    bool rotateString(string s, string goal) {
-4        if(s.size()!=goal.size()){
-5            return false;
-6        }
-7
+4
+5        if(s.size()!=goal.size()){
+6            return false;
+7        }
 8
 9        for(int i=0;i<s.size();i++){
 10            if(s==goal){
 11                return true;
 12                break;
 13            }
-14            char first=s[0];
-15            s.erase(0,1);
-16            s.push_back(first);
-17
+14
+15            char first=s[0];
+16            s.erase(0,1);
+17            s.push_back(first);
 18        }
 19
 20        return false;
