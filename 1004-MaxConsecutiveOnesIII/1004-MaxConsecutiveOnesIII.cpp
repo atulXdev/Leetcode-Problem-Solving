1840b2735cbec95484025d7e3727d8cc8bd5d337
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-22 00:07:11 +0530

    File updated at 22/05/2026, 00:06:43

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.cpp
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.cpp
@@ -1,4 +1,4 @@
-// Last updated: 21/05/2026, 23:03:22
+// Last updated: 22/05/2026, 00:06:43
 1class Solution {
 2public:
 3    int longestOnes(vector<int>& nums, int k) {
@@ -8,25 +8,23 @@
 7        int maxi=0;
 8
 9        while(right<nums.size()){
-10
-11            if(nums[right]==0){
-12                zeroCount++;
-13            }
-14
-15            while(zeroCount>k){
-16                if(nums[left]==0){
-17                    zeroCount--;
-18                }
-19                left++;
-20            }
-21
-22            maxi=max(maxi,right-left+1);
-23
-24            right++;
+10            if(nums[right]==0){
+11                zeroCount++;
+12            }
+13
+14            while(zeroCount>k){
+15                if(nums[left]==0){
+16                    zeroCount--;
+17                }
+18                left++;
+19            }
+20
+21            maxi=max(maxi,right-left+1);
+22
+23            right++;
+24        }
 25
-26        }
-27
-28        return maxi;
-29        
-30    }
-31};
+26        return maxi;
+27        
+28    }
+29};
