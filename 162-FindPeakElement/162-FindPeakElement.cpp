f3cfb3221b238ddc0cd8655698861443fa7ed868
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-23 23:56:05 +0530

    File updated at 23/05/2026, 23:55:36

--- 162-FindPeakElement/162-FindPeakElement.cpp
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -1,13 +1,13 @@
-// Last updated: 08/05/2026, 14:06:36
+// Last updated: 23/05/2026, 23:55:36
 1class Solution {
 2public:
 3    int findPeakElement(vector<int>& nums) {
-4        int n=nums.size();
-5        //if one element
+4
+5        int n=nums.size();
 6        if(n==1){
 7            return 0;
 8        }
-9        //Edge case where fisrt or last elemnt can be peak
+9
 10        if(nums[0]>nums[1]){
 11            return 0;
 12        }
@@ -18,25 +18,27 @@
 17
 18        int low=1;
 19        int high=n-2;
-20        while(low<=high){
-21            int mid=(low+high)/2;
-22            if(nums[mid]>nums[mid-1]&& nums[mid]>nums[mid+1]){
-23                return mid;
-24            }
-25            //if mid on increasing curve then trim the left half
-26
-27            else if(nums[mid]>nums[mid-1]){
-28                low=mid+1;
+20
+21        while(low<=high){
+22            int mid=(low+high)/2;
+23
+24            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+25                return mid;
+26            }
+27
+28            //inc peak
 29
-30            }
-31
-32            else{
+30            else if(nums[mid]>nums[mid-1]){
+31                low=mid+1;
+32            }
 33
-34                high=mid-1;
-35            }
-36        }
-37
-38        return -1;  
-39        
-40    }
-41};
+34            else{
+35                high=mid-1;
+36            }
+37        }
+38
+39        return -1;
+40
+41        
+42    }
+43};
