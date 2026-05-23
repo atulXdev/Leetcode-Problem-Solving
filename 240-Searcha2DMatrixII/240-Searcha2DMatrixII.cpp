4cb60fa9a344942289ea76c3622c6a7698ee8c2b
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-23 22:42:50 +0530

    Create 240-Searcha2DMatrixII

---
+++ 240-Searcha2DMatrixII/240-Searcha2DMatrixII.cpp
@@ -0,0 +1,41 @@
+// Last updated: 23/05/2026, 22:42:20
+1class Solution {
+2public:
+3 bool bs(vector<int>& arr,int aim){
+4    int low=0;
+5    int n=arr.size();
+6    int high=n-1;
+7
+8    while(low<=high){
+9        int mid=(low+high)/2;
+10
+11        if(arr[mid]==aim){
+12            return true;
+13        }
+14
+15        else if(arr[mid]>aim){
+16            high=mid-1;
+17        }
+18
+19        else{
+20            low=mid+1;
+21        }
+22    }
+23
+24    return false;
+25
+26   }
+27    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+28        int m=matrix.size();
+29
+30        for(int i=0;i<m;i++){
+31            bool ans=bs(matrix[i],target);
+32            if(ans){
+33                return true;
+34            }
+35        }
+36
+37        return false;
+38        
+39    }
+40};
