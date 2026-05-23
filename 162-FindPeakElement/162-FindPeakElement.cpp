7d5e15e1679f728a4373b394ed09e8a3154e7dff
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-24 00:09:43 +0530

    File updated at 24/05/2026, 00:09:13

--- 162-FindPeakElement/162-FindPeakElement.cpp
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -1,44 +1,38 @@
-// Last updated: 23/05/2026, 23:55:36
+// Last updated: 24/05/2026, 00:09:13
 1class Solution {
 2public:
 3    int findPeakElement(vector<int>& nums) {
-4
-5        int n=nums.size();
+4        int n=nums.size();
+5
 6        if(n==1){
 7            return 0;
 8        }
-9
-10        if(nums[0]>nums[1]){
-11            return 0;
-12        }
-13
-14        if(nums[n-1]>nums[n-2]){
-15            return n-1;
-16        }
-17
-18        int low=1;
-19        int high=n-2;
+9        if(nums[0]>nums[1]){
+10            return 0;
+11        }
+12        if(nums[n-1]>nums[n-2]){
+13            return n-1;
+14        }
+15
+16        int low=1;
+17        int high=n-2;
+18        while(low<=high){
+19            int mid=(low+high)/2;
 20
-21        while(low<=high){
-22            int mid=(low+high)/2;
-23
-24            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
-25                return mid;
-26            }
-27
-28            //inc peak
-29
-30            else if(nums[mid]>nums[mid-1]){
-31                low=mid+1;
-32            }
+21            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+22                return mid;
+23            }
+24
+25            else if(nums[mid]>nums[mid-1]){
+26                low=mid+1;
+27            }
+28
+29            else{
+30                high=mid-1;
+31            }
+32        }
 33
-34            else{
-35                high=mid-1;
-36            }
-37        }
-38
-39        return -1;
-40
-41        
-42    }
-43};
+34        return -1;
+35        
+36    }
+37};
