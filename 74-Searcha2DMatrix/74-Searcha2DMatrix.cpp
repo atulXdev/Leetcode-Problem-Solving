4a4a0049c867f3b7e69e29ec6d9dd0d8d9152dd8
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-24 00:23:07 +0530

    File updated at 24/05/2026, 00:22:38

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
@@ -1,30 +1,33 @@
-// Last updated: 23/05/2026, 22:36:00
+// Last updated: 24/05/2026, 00:22:38
 1class Solution {
 2public:
 3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
 4        int m=matrix.size();
 5        int n=matrix[0].size();
-6        int low=0;
-7        int high=m*n-1;
-8
-9        while(low<=high){
-10            int mid=(low+high)/2;
-11
-12            int row=mid/n;
-13            int col=mid%n;
-14
-15            if(matrix[row][col]==target){
-16                return true;
-17            }
-18
-19            else if(matrix[row][col]>target){
-20                high=mid-1;
-21            }
-22            else{
-23                low=mid+1;
-24            }
-25        }
-26
-27        return false;
-28    }
-29};
+6
+7        int low=0;
+8        int high=m*n-1;
+9
+10        while(low<=high){
+11            int mid=(low+high)/2;
+12
+13            int row=mid/n;
+14            int col=mid%n;
+15
+16            if(matrix[row][col]==target){
+17                return true;
+18            }
+19
+20            else if(matrix[row][col]<target){
+21                low=mid+1;
+22            }
+23
+24            else{
+25                high=mid-1;
+26            }
+27        }
+28
+29        return false;
+30        
+31    }
+32};
