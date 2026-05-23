a08da23eca63d0ffc8d049e7e951a009364b2d31
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-23 22:36:30 +0530

    File updated at 23/05/2026, 22:36:00

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
@@ -1,41 +1,30 @@
-// Last updated: 23/05/2026, 22:20:34
+// Last updated: 23/05/2026, 22:36:00
 1class Solution {
 2public:
-3   
-4   bool bs(vector<int>& arr,int aim){
-5    int low=0;
-6    int n=arr.size();
-7    int high=n-1;
+3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+4        int m=matrix.size();
+5        int n=matrix[0].size();
+6        int low=0;
+7        int high=m*n-1;
 8
-9    while(low<=high){
-10        int mid=(low+high)/2;
+9        while(low<=high){
+10            int mid=(low+high)/2;
 11
-12        if(arr[mid]==aim){
-13            return true;
-14        }
-15
-16        else if(arr[mid]>aim){
-17            high=mid-1;
-18        }
-19
-20        else{
-21            low=mid+1;
-22        }
-23    }
-24
-25    return false;
+12            int row=mid/n;
+13            int col=mid%n;
+14
+15            if(matrix[row][col]==target){
+16                return true;
+17            }
+18
+19            else if(matrix[row][col]>target){
+20                high=mid-1;
+21            }
+22            else{
+23                low=mid+1;
+24            }
+25        }
 26
-27   }
-28    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-29        int m=matrix.size();
-30        
-31        for(int i=0;i<m;i++){
-32            bool ans=bs(matrix[i],target);
-33            if(ans==true){
-34                return true;
-35            }
-36        }
-37
-38        return false;
-39    }
-40};
+27        return false;
+28    }
+29};
