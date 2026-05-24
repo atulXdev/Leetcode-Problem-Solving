527286378de316c793be5149736009c8f338e6bc
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-24 23:01:34 +0530

    File updated at 24/05/2026, 23:01:03

--- 202-HappyNumber/202-HappyNumber.cpp
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -1,30 +1,20 @@
-// Last updated: 24/05/2026, 22:49:18
+// Last updated: 24/05/2026, 23:01:03
 1class Solution {
 2public:
-3    int sumsq(int x) {
-4        int sum = 0;
-5
-6        while (x > 0) {
-7            int last = x % 10;
-8            sum += last * last;
-9            x /= 10;
-10        }
+3    int maxProfit(vector<int>& prices) {
+4        int minP=INT_MAX;
+5        int maxP=0;
+6
+7        for(int price:prices){
+8            if(price<minP){
+9                minP=price;
+10            }
 11
-12        return sum;
-13    }
-14
-15    bool isHappy(int n) {
-16        unordered_set<int> st;
-17
-18        while (n != 1) {
-19            if (st.count(n)) {
-20                return false;
-21            }
-22
-23            st.insert(n);
-24            n = sumsq(n);
-25        }
-26
-27        return true;
-28    }
-29};
+12            if(price-minP>maxP){
+13                maxP=price-minP;
+14            }
+15        }
+16
+17        return maxP;
+18    }
+19};
