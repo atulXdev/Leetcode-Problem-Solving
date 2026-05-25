da59da8e92a7dc982e89635c5017dfa58226475d
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-25 10:21:15 +0530

    File updated at 25/05/2026, 10:20:45

--- 20-ValidParentheses/20-ValidParentheses.cpp
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -1,29 +1,18 @@
-// Last updated: 25/05/2026, 10:03:53
+// Last updated: 25/05/2026, 10:20:45
 1class Solution {
 2public:
-3    string addBinary(string a, string b) {
-4        string ans="";
-5        int i=a.size()-1;
-6        int j=b.size()-1;
-7        int carry=0;
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int,int> mp;
+5
+6        for(int i=0;i<nums.size();i++){
+7            int need=target-nums[i];
 8
-9        while(i>=0 || j>=0 || carry){
-10            int sum=carry;
-11
-12            if(i>=0){
-13                sum+=a[i--]-'0';
-14            }
-15
-16            if(j>=0){
-17                sum+=b[j--]-'0';
-18            }
-19
-20            ans+=(sum%2)+'0';
-21            carry=sum/2;
-22        }
-23
-24        reverse(ans.begin(),ans.end());
-25        return ans;
-26        
-27    }
-28};
+9            if(mp.count(need)){
+10                return {mp[need],i};
+11            }
+12
+13            mp[nums[i]]=i;
+14        }
+15        return {};
+16    }
+17};
