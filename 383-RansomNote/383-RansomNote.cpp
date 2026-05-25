c5255a22e6be2547d61e54afe7d20faf370cec8c
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-25 11:08:37 +0530

    File updated at 25/05/2026, 11:08:06

--- 383-RansomNote/383-RansomNote.cpp
+++ 383-RansomNote/383-RansomNote.cpp
@@ -1,22 +1,27 @@
-// Last updated: 25/05/2026, 10:41:25
+// Last updated: 25/05/2026, 11:08:06
 1class Solution {
 2public:
-3    bool canConstruct(string ransomNote, string magazine) {
-4        
-5        vector<int> freq(26,0);
+3    string mergeAlternately(string word1, string word2) {
+4        int mini = min(word1.size(), word2.size());
+5        string ans = "";
 6
-7        for(char ch:magazine){
-8            freq[ch-'a']++;
-9        }
+7        for(int i = 0; i < mini; i++){
+8            ans += word1[i];
+9            ans += word2[i];
 10
-11        for(char ch:ransomNote){
-12            if(freq[ch-'a']==0){
-13                return false;
-14            }
-15
-16            freq[ch-'a']--;
-17        }
-18
-19        return true;
-20    }
-21};
+11            if(i == mini-1 && word1.size() != mini){
+12                for(int j = mini; j < word1.size(); j++){
+13                    ans += word1[j];
+14                }
+15            }
+16
+17            if(i == mini-1 && word2.size() != mini){
+18                for(int j = mini; j < word2.size(); j++){
+19                    ans += word2[j];
+20                }
+21            }
+22        }
+23
+24        return ans;
+25    }
+26};
