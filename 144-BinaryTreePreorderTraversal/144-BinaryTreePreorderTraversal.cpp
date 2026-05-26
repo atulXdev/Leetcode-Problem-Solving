97cb103968a51a80bbf8a9e776aa36d1874f0a91
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-26 22:30:59 +0530

    File updated at 26/05/2026, 22:30:26

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -1,4 +1,4 @@
-// Last updated: 26/05/2026, 15:25:56
+// Last updated: 26/05/2026, 22:30:26
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -11,23 +11,26 @@
 10 * };
 11 */
 12class Solution {
-13    //ROOT LEFT RIGHT
-14public:
-15 void preorder(TreeNode* root,vector<int> & ans){
-16
-17    if(root==NULL){
-18        return;
-19    }
-20    ans.push_back(root->val);
-21    preorder(root->left,ans);
-22    preorder(root->right,ans);
+13public:
+14    vector<int> preorderTraversal(TreeNode* root) {
+15         vector<int> preorder;
+16        if(root==NULL) return preorder;
+17        stack<TreeNode*> st;
+18        st.push(root);
+19
+20        while(!st.empty()){
+21            root=st.top();
+22            st.pop();
 23
-24 }
-25    vector<int> preorderTraversal(TreeNode* root) {
-26        vector<int> ans;
-27        preorder(root,ans);
+24            preorder.push_back(root->val);
+25
+26            if(root->right!=NULL) st.push(root->right);
+27             if(root->left!=NULL) st.push(root->left);
 28
-29        return ans;
-30        
-31    }
-32};
+29
+30        }
+31
+32        return preorder;
+33        
+34    }
+35};
