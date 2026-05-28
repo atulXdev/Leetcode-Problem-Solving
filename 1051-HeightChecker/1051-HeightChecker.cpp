902dba82dd474db9473b03ba437181f1760eb106
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-28 17:59:13 +0530

    File updated at 28/05/2026, 17:58:39

--- 1051-HeightChecker/1051-HeightChecker.cpp
+++ 1051-HeightChecker/1051-HeightChecker.cpp
@@ -1,20 +1,30 @@
-// Last updated: 28/05/2026, 17:45:32
+// Last updated: 28/05/2026, 17:58:39
 1class Solution {
 2public:
-3    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-4        string ans1="";
-5        string ans2="";
-6
-7       for(int i=0;i<word1.size();i++){
-8        ans1+=word1[i];
-9       }
+3    int maxProductDifference(vector<int>& nums) {
+4
+5        int maxPdt=INT_MIN;
+6        int minPdt=INT_MAX;
+7        int maxPdtDiff=0;
+8
+9        for(int i=0;i<nums.size();i++){
 10
-11       for(int i=0;i<word2.size();i++){
-12        ans2+=word2[i];
-13       }
-14       
-15
-16        return ans1==ans2;
-17        
-18    }
-19};
+11            for(int j=i+1;j<nums.size();j++){
+12                long long pdt=nums[i]*nums[j];
+13
+14                if(pdt>maxPdt){
+15                    maxPdt=pdt;
+16                }
+17
+18                if(pdt<minPdt){
+19                    minPdt=pdt;
+20                }
+21            }
+22            maxPdtDiff=max(maxPdtDiff,maxPdt-minPdt);
+23        }
+24
+25        return maxPdtDiff;
+26
+27        
+28    }
+29};
