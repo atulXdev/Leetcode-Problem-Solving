ac8db2830e6d78e855eebe715d38a36a0809c90f
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-29 00:25:18 +0530

    File updated at 29/05/2026, 00:24:42

--- 1051-HeightChecker/1051-HeightChecker.cpp
+++ 1051-HeightChecker/1051-HeightChecker.cpp
@@ -1,30 +1,20 @@
-// Last updated: 28/05/2026, 17:58:39
+// Last updated: 29/05/2026, 00:24:42
 1class Solution {
 2public:
-3    int maxProductDifference(vector<int>& nums) {
+3    int heightChecker(vector<int>& heights) {
 4
-5        int maxPdt=INT_MIN;
-6        int minPdt=INT_MAX;
-7        int maxPdtDiff=0;
+5        vector<int> normal(heights.begin(),heights.end());
+6        sort(heights.begin(),heights.end());
+7        int count=0;
 8
-9        for(int i=0;i<nums.size();i++){
-10
-11            for(int j=i+1;j<nums.size();j++){
-12                long long pdt=nums[i]*nums[j];
+9        for(int i=0;i<normal.size();i++){
+10            if(heights[i]!=normal[i]){
+11                count++;
+12            }
 13
-14                if(pdt>maxPdt){
-15                    maxPdt=pdt;
-16                }
-17
-18                if(pdt<minPdt){
-19                    minPdt=pdt;
-20                }
-21            }
-22            maxPdtDiff=max(maxPdtDiff,maxPdt-minPdt);
-23        }
-24
-25        return maxPdtDiff;
-26
-27        
-28    }
-29};
+14        }
+15
+16        return count;
+17        
+18    }
+19};
