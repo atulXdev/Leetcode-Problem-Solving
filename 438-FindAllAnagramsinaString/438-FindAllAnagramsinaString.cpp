2a967f766c23db565f7f32f192092385ef457ca7
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-29 00:17:51 +0530

    File updated at 29/05/2026, 00:17:15

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.cpp
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.cpp
@@ -1,11 +1,11 @@
-// Last updated: 28/05/2026, 23:35:23
+// Last updated: 29/05/2026, 00:17:15
 1class Solution {
 2public:
 3    vector<int> findAnagrams(string s, string p) {
-4
-5        vector<int> ans;
-6        int k=p.size();
-7        int left=0;
+4        int k=p.size();
+5        int left=0;
+6        vector<int> ans;
+7
 8        vector<int> freqp(26,0);
 9        vector<int> freqs(26,0);
 10
@@ -21,15 +21,12 @@
 20                left++;
 21            }
 22
-23            if(right-left+1==k && freqp==freqs){
+23            if(right-left+1==k && freqs==freqp){
 24                ans.push_back(left);
 25            }
 26        }
 27
 28        return ans;
-29
-30
-31
-32        
-33    }
-34};
+29        
+30    }
+31};
