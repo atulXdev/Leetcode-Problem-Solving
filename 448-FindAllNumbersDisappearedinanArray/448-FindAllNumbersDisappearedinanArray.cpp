b10bfdf81538c327cc6718c91b0f3eda95340e9d
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-05-29 23:29:19 +0530

    Create 448-FindAllNumbersDisappearedinanArray

---
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.cpp
@@ -0,0 +1,22 @@
+// Last updated: 29/05/2026, 23:28:43
+1class Solution {
+2public:
+3    vector<int> findDisappearedNumbers(vector<int>& nums) {
+4
+5        for(int i = 0; i < nums.size(); i++) {
+6            int idx = abs(nums[i]) - 1;
+7
+8            if(nums[idx] > 0)
+9                nums[idx] = -nums[idx];
+10        }
+11
+12        vector<int> ans;
+13
+14        for(int i = 0; i < nums.size(); i++) {
+15            if(nums[i] > 0)
+16                ans.push_back(i + 1);
+17        }
+18
+19        return ans;
+20    }
+21};
