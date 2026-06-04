20beaf3bed41cb257ce1370c2b49f3e8ea6529d3
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-06-04 17:25:20 +0530

    File updated at 04/06/2026, 17:24:40

--- 344-ReverseString/344-ReverseString.cpp
+++ 344-ReverseString/344-ReverseString.cpp
@@ -1,15 +1,17 @@
-// Last updated: 04/06/2026, 17:13:34
+// Last updated: 04/06/2026, 17:24:40
 1class Solution {
 2public:
-3    void reverseString(vector<char>& s) {
-4        int left=0;
-5        int right=s.size()-1;
-6
-7        while(left<right){
-8            swap(s[left],s[right]);
-9            left++;
-10            right--;
-11        }
-12        
-13    }
-14};
+3    vector<int> sortedSquares(vector<int>& nums) {
+4        multiset<int> ms;
+5        for(int i=0;i<nums.size();i++){
+6            int num=abs(nums[i]*nums[i]);
+7            ms.insert(num);
+8        }
+9
+10        vector<int> ans(ms.begin(),ms.end());
+11
+12        return ans;
+13
+14        
+15    }
+16};
