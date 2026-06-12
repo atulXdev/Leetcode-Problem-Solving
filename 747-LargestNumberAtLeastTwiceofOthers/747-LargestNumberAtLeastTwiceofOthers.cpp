ac5a1bc40afae2b5d241d38889b561c0dd0c2cbc
Atul Singh <atul.kumar.singh.cg@gmail.com>
2026-06-12 23:58:22 +0530

    File updated at 12/06/2026, 23:57:37

--- 747-LargestNumberAtLeastTwiceofOthers/747-LargestNumberAtLeastTwiceofOthers.cpp
+++ 747-LargestNumberAtLeastTwiceofOthers/747-LargestNumberAtLeastTwiceofOthers.cpp
@@ -1,22 +1,34 @@
-// Last updated: 12/06/2026, 23:31:50
+// Last updated: 12/06/2026, 23:57:37
 1class Solution {
 2public:
-3    int dominantIndex(vector<int>& nums) {
-4        int maxi=*max_element(nums.begin(),nums.end());
-5        int index;
-6
-7        for(int i=0;i<nums.size();i++){
-8            if(nums[i]==maxi){
-9                index=i;
-10                continue;
-11                
-12            }
-13            if(!(nums[i]*2<=maxi)){
-14                return -1;
-15            }
+3    vector<string> findRelativeRanks(vector<int>& score) {
+4
+5        vector<int> real(score.begin(),score.end());
+6        vector<string> ans;
+7        sort(score.begin(),score.end(), greater<int>());
+8        map<int,string> mp;
+9       for(int i=0;i<score.size();i++){
+10        if(i==0){
+11            mp[score[i]]="Gold Medal";
+12        }
+13
+14      else  if(i==1){
+15            mp[score[i]]="Silver Medal";
 16        }
 17
-18        return index;
-19        
-20    }
-21};
+18       else  if(i==2){
+19            mp[score[i]]="Bronze Medal";
+20        }
+21        else{
+22            
+23            mp[score[i]]=to_string(i+1);
+24       } }
+25
+26       for(int n:real){
+27        ans.push_back(mp[n]);
+28       }
+29
+30       return ans;
+31        
+32    }
+33};
